acf3915666bd4cafd0ed1cb319e7704bc0331c69
7aib <zohaibrana03@gmail.com>
2025-08-17 00:46:03 +0500

    feat: working on live feed

---
+++ mobile-app/URL_CONSTANTS.js
@@ -0,0 +1,1 @@
+LIVE_FEED_URL = "http://192.168.1.8:8000/api/places/feed/";
--- mobile-app/screens/HomeScreen.js
+++ mobile-app/screens/HomeScreen.js
@@ -1,33 +1,66 @@
-import { FlatList, View, StyleSheet } from 'react-native';
-import ReelCard from '../components/ReelCard';
-
-const dummyReels = [
-  {
-    id: '1',
-    caption: 'Exploring northern Pakistan 🌄',
-    thumbnail: 'https://source.unsplash.com/featured/?mountains,pakistan',
-  },
-  {
-    id: '2',
-    caption: 'Karachi street food vibes 🍢',
-    thumbnail: 'https://source.unsplash.com/featured/?karachi,food',
-  },
-  {
-    id: '3',
-    caption: 'Camping under the stars ✨',
-    thumbnail: 'https://source.unsplash.com/featured/?camping,stars',
-  },
-];
+import {
+  FlatList,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Text,
+} from "react-native";
+import ReelCard from "../components/ReelCard";
+import LIVE_FEED_URL from "../URL_CONSTANTS";
+import { useEffect, useState } from "react";
 
 const HomeScreen = () => {
+  const [reels, setReels] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://192.168.1.8:8000")
+      .then((res) => console.log("✅ Connected!", res.status))
+      .catch((err) => console.log("❌ Failed", err));
+    //fetchReels();
+  }, []);
+  const fetchReels = async () => {
+    try {
+      const response = await fetch(LIVE_FEED_URL);
+      console.log("Response from backend:", response);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+
+      // Format backend response for ReelCard
+      const formatted = data.map((item) => ({
+        id: item.id.toString(),
+        caption: item.title || item.place_name, // fallback if title is null
+        thumbnail: item.url, // this is actually a video url
+      }));
+
+      setReels(formatted);
+    } catch (error) {
+      console.error("Error fetching reels:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color="#fff" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={dummyReels}
+        data={reels}
         renderItem={({ item }) => <ReelCard reel={item} />}
         keyExtractor={(item) => item.id}
         pagingEnabled
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <Text style={{ color: "white" }}>No reels available</Text>
+        }
       />
     </View>
   );
@@ -36,7 +69,7 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
 });
 
